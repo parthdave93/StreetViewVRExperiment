b4433fc896e8f76893b6371527ee57237ef47cca
Parth Dave <parth.dave93@gmail.com>
2017-05-03 08:27:54 +0530

    Dependencies adding

---
+++ app/src/main/java/com/parthdave/streetviewandvr/StreetViewActivity.java
@@ -0,0 +1,13 @@
+package com.parthdave.streetviewandvr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StreetViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_street_view);
+    }
+}
