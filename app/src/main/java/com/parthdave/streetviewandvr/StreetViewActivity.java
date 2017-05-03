8de9716b614d171cf651a71e551d2f50d16ca1f5
Parth Dave <parth.dave93@gmail.com>
2017-05-03 15:32:20 +0530

    street view done

--- app/src/main/java/com/parthdave/streetviewandvr/StreetViewActivity.java
+++ app/src/main/java/com/parthdave/streetviewandvr/StreetViewActivity.java
@@ -3,11 +3,33 @@ package com.parthdave.streetviewandvr;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class StreetViewActivity extends AppCompatActivity {
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
+import com.google.android.gms.maps.StreetViewPanoramaOptions;
+import com.google.android.gms.maps.StreetViewPanoramaView;
+import com.google.android.gms.maps.model.LatLng;
 
+public class StreetViewActivity extends AppCompatActivity implements OnStreetViewPanoramaReadyCallback {
+    
+    StreetViewPanoramaFragment streetViewPanoramaFragment;
+    StreetViewPanoramaView mStreetViewPanoramaView;
+            
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_street_view);
+        
+        streetViewPanoramaFragment = (StreetViewPanoramaFragment) getFragmentManager().findFragmentById(R.id.streetviewpanorama);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
+        
+    }
+    
+    @Override
+    public void onStreetViewPanoramaReady(StreetViewPanorama streetViewPanorama) {
+        LatLng SAN_FRAN = new LatLng(-33.87365, 151.20689);
+        streetViewPanorama.setPosition(SAN_FRAN);
+        mStreetViewPanoramaView = new StreetViewPanoramaView(this, new StreetViewPanoramaOptions().position(SAN_FRAN));
+        
     }
 }
